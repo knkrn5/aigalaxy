8955ae95d8602a91f1c43dff22b6404cadbb2f45
karan <xkaranx5@gmail.com>
2025-08-01 10:46:23 +0530

    feat: add health check endpoint to main application

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,10 +3,16 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routes.aichats_routes import router as aichatsroutes
+import os
+
+is_production = os.getenv("ENV") == "PRODUCTION"
+print(is_production)
 
 
 origins = [
     "http://localhost:5173",
+] if not is_production else [
+    "https://your-production-domain.com",
 ]
 
 
@@ -35,6 +41,11 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/health")
+def read_health():
+    return {"status": "healthy"}
 
 
 app.include_router(aichatsroutes, prefix="/aichats", tags=["AI Chat"])
